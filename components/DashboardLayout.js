50c9ab8739f6eb963602ade088f08e74f182bc64
ninacarlas <nina.frani00@gmail.com>
2024-03-04 14:28:29 -0700

    dashboardlayout error build

--- components/DashboardLayout.js
+++ components/DashboardLayout.js
@@ -20,40 +20,4 @@ export default function DashboardLayout({ children }) {
     </Box>
   );
 }
-  // return (
-  //   <Box sx={{ display: 'flex' }}>
-  //     <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-  //       <Toolbar>
-  //         <Typography variant="h6" noWrap component="div">
-  //           Mayvis
-  //         </Typography>
-  //       </Toolbar>
-  //     </AppBar>
-  //     <Drawer
-  //       variant="permanent"
-  //       sx={{
-  //         width: drawerWidth,
-  //         flexShrink: 0,
-  //         [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
-  //       }}
-  //     >
-  //       <Toolbar />
-  //       <Box sx={{ overflow: 'auto' }}>
-  //           <List>
-  //             {['Home', 'Proposals'].map((text, index) => (
-  //               <ListItem button key={text}>
-  //                 <ListItemIcon>
-  //                   {index % 2 === 0 ? <HomeIcon /> : <InfoIcon />}
-  //                 </ListItemIcon>
-  //                 <ListItemText primary={text} />
-  //               </ListItem>
-  //             ))}
-  //           </List>
-  //         </Box>
-  //     </Drawer>
-  //     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-  //       <Toolbar />
-  //       {children}
-  //     </Box>
-  //   </Box>
-  // ); 
+  
