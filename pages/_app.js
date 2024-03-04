02971a6b2a7d0bc8e00c89ad97fcc1d8c9132f78
ninacarlas <nina.frani00@gmail.com>
2024-03-04 14:37:52 -0700

    dashboardlayout error build

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,16 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import DashboardLayout from '@component/DashboardLayout';
 
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps, ...appProps }) {
   
   if(['/login'].includes(appProps.router.pathname))
-    return <Component {...pageProps} />;
-
   return (
+    <DashboardLayout>
       <Component {...pageProps} />
+    </DashboardLayout>
   );
 }
