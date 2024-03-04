334cc35a87e14f0695cb005ad545ab366b873cbb
ninacarlas <nina.frani00@gmail.com>
2024-03-04 15:53:29 -0700

    dashboardlayout error build

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import DashboardLayout from '@/components/Dashboardlayout';
+import DashboardLayout from '@/components/DashboardLayout';
 
 import '@/styles/globals.css'
 
